eceae8f3a36bd30bd53f5ff285b9e665b9f78f40
amiruddin1 <67670878+amiruddin1@users.noreply.github.com>
2020-07-06 08:22:58 +0530

    Create loopprogram.java

---
+++ loopprogram.java
@@ -0,0 +1,21 @@
+Public class loopprogram{
+Public static void main(string[] args)
+{ 
+      Int i = 10;
+      While (i>0)
+      {
+        System.out.println(i);
+        i--;
+      }
+      Int a=0;
+      Do 
+      {
+        System.out.println("... ");
+      }
+      While(a<10);
+      For(int b=0; b<10; b++)
+      {
+        System.out.println("I is: "+i);
+      }
+}
+}
